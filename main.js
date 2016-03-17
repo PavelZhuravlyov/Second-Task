c2e2e60a825b190c04847a73566cddd7897164f4
Павел Журавлёв < >
2016-03-17 13:53:39 +0500

    Second Task Main

---
+++ main.js
@@ -0,0 +1,214 @@
+$(document).ready(function(){
+
+	(function(){
+		var mrgPopUp = $('.filter-popup').css('marginTop');
+
+		$(document).on('click', '.btn-filter', function(){
+			$('body').addClass('bodyShadow');
+			$('.filter-popup').fadeIn(300);
+			$('.filter-popup').css({
+				'marginTop': -200
+			});
+			return false;
+		});
+
+		$(document).on('click', '.close-popup', function(){
+			closePopUp(mrgPopUp);
+			return false;
+		});
+
+		$(document).click(function (e){
+			var div = $('.filter-popup');
+			if (!div.is(e.target) && div.has(e.target).length === 0) {
+				closePopUp(mrgPopUp);
+			}
+		});
+
+		$(document).on('click', '.btn-clear-filter', function(){
+			document.location.href = '/';
+			return false;
+		});
+	})();
+
+	(function(){
+		var list = ponyList.pony;
+		var filter = parseUrl(decodeURI(document.location.search));
+		
+		(!filter) ? document.getElementById('id01').innerHTML = outPut(list, outRand(list.length-1)) : document.getElementById('id01').innerHTML = printGoodsFilter(list, renderFilter(list, filter));
+	})();
+
+	function printGoodsFilter(list, arrNumbers){
+		var stringGoods = '';
+		for(var i=0; i<arrNumbers.length; i++){
+			stringGoods += "<div class=\"item\"><p>Название: " + list[arrNumbers[i]].name + "</p><p>Цвет: " + list[arrNumbers[i]].color + "</p><p>Вид: " + list[arrNumbers[i]].kind + "</p><p>Цена: " + list[arrNumbers[i]].price + "</p><p>Новый товар: " + list[arrNumbers[i]].is_new + "</p></div>";
+		}
+		return stringGoods;
+	}
+
+	function renderFilter(list, filter){
+		var arrCountsOk = [],
+			arrMainCounts = [],
+			arrBuf = [];
+
+		if(filter.color.length != 0){
+			for(var i=0; i<list.length; i++){
+				for(var z=0; z<filter.color.length; z++){
+					if(list[i].color == filter.color[z]){
+						arrCountsOk.push(i); // массив с индексами элементов, которые подходят по цвету
+					} 
+				}
+			}
+
+			arrBuf = arrCountsOk;
+			arrMainCounts = arrBuf;
+			arrCountsOk = [];
+		}
+
+		if(filter.kind.length != 0){
+			for(var z=0; z<arrBuf.length; z++){
+				for(var q=0; q<filter.kind.length; q++){
+					if(list[arrBuf[z]].kind == filter.kind[q]){
+						arrCountsOk.push(arrBuf[z]);
+					}
+				} 
+			}
+
+			arrBuf = arrCountsOk;
+			arrMainCounts = arrBuf;
+			arrCountsOk = [];
+		}
+
+		if(filter.is_new.length != 0){
+			for(var z=0; z<arrBuf.length; z++){
+				for(var q=0; q<filter.is_new.length; q++){
+					if(String(list[arrBuf[z]].is_new) == filter.is_new[q]){
+						arrCountsOk.push(arrBuf[z]);
+					}
+				} 
+			}
+
+			arrBuf = arrCountsOk;
+			arrMainCounts = arrBuf;
+			arrCountsOk = [];
+		}
+
+		if(filter.from[0] != '' && filter.to[0] != ''){
+			var fromPrice = filter.from[0],
+				toPrice = filter.to[0];
+
+			for(var z=0; z<arrBuf.length; z++){
+				if(list[arrBuf[z]].price > parseFloat(fromPrice) && list[arrBuf[z]].price < parseFloat(toPrice)) {
+					arrCountsOk.push(arrBuf[z]);
+				}
+			}
+		}
+
+		return arrMainCounts;
+	}
+
+	function count_prs(obj){
+	    var count = 0; 
+	    for(var prs in obj) count++; 
+	    return count; 
+	}
+
+	function parseUrl(url){
+		if(url.indexOf('?') < 0) return false;
+
+		url = url.split('?'); 
+		url = url[1]; 
+
+		return createHash(url);
+	}
+
+	function createHash(str){
+		var obj = {
+			'color': [],
+			'from': [],
+			'to': [],
+			'kind': [],
+			'is_new': []
+		};
+
+		var symbolPosition = 0;
+		var substr = '';
+		var property = '';
+		var arr = [];
+		var countSymb = countSymbols(str, '&');
+
+		for(var i=0; i<countSymb; i++){
+			symbolPosition = str.indexOf('&');
+			substr = str.slice(0, symbolPosition);
+
+			property = substr.slice(0, substr.indexOf('='));
+			obj[property].push(substr.slice(substr.indexOf('=')+1, substr.length));
+
+			str = str.replace(substr, '');
+			if(i != (countSymb-1)) str = str.replace('&', '');
+		}
+
+		return obj;
+	}
+
+	function countSymbols(str, symbol){
+		var count = 0;
+		for(var i=0; i<str.length; i++) if(str[i] == symbol) count++;
+		return count;
+	}
+
+	function findSymbol(str, symbol){
+		return str.indexOf(symbol);
+	}
+
+	function sorting(a,b){
+		return a - b;
+	}
+
+	function outPut(list, arrNumbers){ // Ещё как параметр массив со случ числами
+		var stringGoods = "";
+		arrNumbers = arrNumbers.sort(sorting);
+
+		for(var i=0; i<arrNumbers.length; i++){
+			stringGoods += "<div class=\"item\"><p>Название: " + list[arrNumbers[i]].name + "</p><p>Цвет: " + list[arrNumbers[i]].color + "</p><p>Вид: " + list[arrNumbers[i]].kind + "</p><p>Цена: " + list[arrNumbers[i]].price + "</p><p>Новый товар: " + list[arrNumbers[i]].is_new + "</p></div>";
+		}
+
+		return stringGoods;
+	}
+
+	function outRand(listCount){
+		var arrNumbers = [],
+			j = 0;
+
+		while(j != 20){
+			var genNumber = Math.floor((Math.random() * listCount) + 0);
+			if(searchNumbInArr(genNumber, arrNumbers) != true) {
+				arrNumbers.push(genNumber);
+				j++;
+			}
+		}
+
+		return arrNumbers;
+	}
+
+	function searchNumbInArr(numb, arr){
+		var j = 0;
+
+		for(var i=0; i<arr.length; i++){
+			if(numb == arr[i]) {
+				j++;
+				break;
+			}
+		}
+
+		if(j == 0) return false;
+		else return true;
+	}
+
+	function closePopUp(mrgPopUp){
+		$('body').removeClass('bodyShadow');
+		$('.filter-popup').fadeOut(200);
+		$('.filter-popup').css({
+			'marginTop': mrgPopUp
+		});
+	}
+});
---
+++ store.js
@@ -0,0 +1,280 @@
+var ponyList = {
+	"pony": [
+		{
+			"name": "Товар1",
+			"color": "Фиолетовый",
+			"kind": "Единорог",
+			"price": 19.99,
+			"is_new": false
+		},
+		{
+			"name": "Товар2",
+			"color": "Синий",
+			"kind": "Лошадь",
+			"price": 10,
+			"is_new": false
+		},
+		{
+			"name": "Товар3",
+			"color": "Жёлтый",
+			"kind": "Единорог",
+			"price": 150,
+			"is_new": true
+		},
+		{
+			"name": "Товар4",
+			"color": "Зелёный",
+			"kind": "Лошадь",
+			"price": 20.45,
+			"is_new": true
+		},
+		{
+			"name": "Товар5",
+			"color": "Белый",
+			"kind": "Единорог",
+			"price": 50,
+			"is_new": true
+		},
+		{
+			"name": "Товар6",
+			"color": "Синий",
+			"kind": "Единорог",
+			"price": 500,
+			"is_new": false
+		},
+		{
+			"name": "Товар7",
+			"color": "Серый",
+			"kind": "Лошадь",
+			"price": 3.94,
+			"is_new": false
+		},{
+			"name": "Товар8",
+			"color": "Чёрный",
+			"kind": "Единорог",
+			"price": 150,
+			"is_new": true
+		},
+		{
+			"name": "Товар9",
+			"color": "Красный",
+			"kind": "Единорог",
+			"price": 30,
+			"is_new": false
+		},
+		{
+			"name": "Товар10",
+			"color": "Зелёный",
+			"kind": "Лошадь",
+			"price": 80,
+			"is_new": true
+		},
+		{
+			"name": "Товар11",
+			"color": "Фиолетовый",
+			"kind": "Единорог",
+			"price": 19.99,
+			"is_new": false
+		},
+		{
+			"name": "Товар12",
+			"color": "Синий",
+			"kind": "Лошадь",
+			"price": 10,
+			"is_new": false
+		},
+		{
+			"name": "Товар13",
+			"color": "Жёлтый",
+			"kind": "Единорог",
+			"price": 150,
+			"is_new": true
+		},
+		{
+			"name": "Товар14",
+			"color": "Зелёный",
+			"kind": "Лошадь",
+			"price": 20.45,
+			"is_new": true
+		},
+		{
+			"name": "Товар15",
+			"color": "Белый",
+			"kind": "Единорог",
+			"price": 50,
+			"is_new": true
+		},
+		{
+			"name": "Товар16",
+			"color": "Синий",
+			"kind": "Единорог",
+			"price": 500,
+			"is_new": false
+		},
+		{
+			"name": "Товар17",
+			"color": "Серый",
+			"kind": "Лошадь",
+			"price": 3.94,
+			"is_new": false
+		},{
+			"name": "Товар18",
+			"color": "Чёрный",
+			"kind": "Единорог",
+			"price": 150,
+			"is_new": true
+		},
+		{
+			"name": "Товар19",
+			"color": "Красный",
+			"kind": "Единорог",
+			"price": 30,
+			"is_new": false
+		},
+		{
+			"name": "Товар20",
+			"color": "Зелёный",
+			"kind": "Лошадь",
+			"price": 80,
+			"is_new": true
+		},
+		{
+			"name": "Товар21",
+			"color": "Фиолетовый",
+			"kind": "Единорог",
+			"price": 19.99,
+			"is_new": false
+		},
+		{
+			"name": "Товар22",
+			"color": "Синий",
+			"kind": "Лошадь",
+			"price": 10,
+			"is_new": false
+		},
+		{
+			"name": "Товар23",
+			"color": "Жёлтый",
+			"kind": "Единорог",
+			"price": 150,
+			"is_new": true
+		},
+		{
+			"name": "Товар24",
+			"color": "Зелёный",
+			"kind": "Лошадь",
+			"price": 20.45,
+			"is_new": true
+		},
+		{
+			"name": "Товар25",
+			"color": "Белый",
+			"kind": "Единорог",
+			"price": 50,
+			"is_new": true
+		},
+		{
+			"name": "Товар26",
+			"color": "Синий",
+			"kind": "Единорог",
+			"price": 500,
+			"is_new": false
+		},
+		{
+			"name": "Товар27",
+			"color": "Серый",
+			"kind": "Лошадь",
+			"price": 3.94,
+			"is_new": false
+		},{
+			"name": "Товар28",
+			"color": "Чёрный",
+			"kind": "Единорог",
+			"price": 150,
+			"is_new": true
+		},
+		{
+			"name": "Товар29",
+			"color": "Красный",
+			"kind": "Единорог",
+			"price": 30,
+			"is_new": false
+		},
+		{
+			"name": "Товар30",
+			"color": "Зелёный",
+			"kind": "Лошадь",
+			"price": 80,
+			"is_new": true
+		},
+		{
+			"name": "Товар31",
+			"color": "Фиолетовый",
+			"kind": "Единорог",
+			"price": 19.99,
+			"is_new": false
+		},
+		{
+			"name": "Товар32",
+			"color": "Синий",
+			"kind": "Лошадь",
+			"price": 10,
+			"is_new": false
+		},
+		{
+			"name": "Товар33",
+			"color": "Жёлтый",
+			"kind": "Единорог",
+			"price": 150,
+			"is_new": true
+		},
+		{
+			"name": "Товар34",
+			"color": "Зелёный",
+			"kind": "Лошадь",
+			"price": 20.45,
+			"is_new": true
+		},
+		{
+			"name": "Товар35",
+			"color": "Белый",
+			"kind": "Единорог",
+			"price": 50,
+			"is_new": true
+		},
+		{
+			"name": "Товар36",
+			"color": "Синий",
+			"kind": "Единорог",
+			"price": 500,
+			"is_new": false
+		},
+		{
+			"name": "Товар37",
+			"color": "Серый",
+			"kind": "Лошадь",
+			"price": 3.94,
+			"is_new": false
+		},{
+			"name": "Товар38",
+			"color": "Чёрный",
+			"kind": "Единорог",
+			"price": 150,
+			"is_new": true
+		},
+		{
+			"name": "Товар39",
+			"color": "Красный",
+			"kind": "Лошадь",
+			"price": 40,
+			"is_new": false
+		},
+		{
+			"name": "Товар40",
+			"color": "Зелёный",
+			"kind": "Лошадь",
+			"price": 80,
+			"is_new": true
+		}
+	]
+};
